e38801e2384342c1fecf73645f531aa283bebf86
Sidney Uwaya <uwayasidney@gmail.com>
2022-08-19 23:23:39 +0100

    update

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector('.mob-menu')
 
 const menu = document.querySelector('nav')
-const body = document.querySelector('body')
 const menuOpen = document.querySelector('.menu-open')
 const menuClose = document.querySelector('.times')
 const triangle = document.querySelector('.triangle')
